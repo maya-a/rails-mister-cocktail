d93ca25f8adf8faa4c3a539b0b1cf3ff15214680
maya aderka <mayaad0@gmail.com>
2019-06-19 16:31:39 +0300

    done with ne css

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,13 +1,11 @@
 class CocktailsController < ApplicationController
-  # before_action :find_cocktail, only: [:show]
+  before_action :find_cocktail, only: [:show, :destroy]
 
   def index
     @cocktails = Cocktail.all
   end
 
-  def show
-    @cocktail = Cocktail.find(params[:id])
-  end
+  def show; end
 
   def new
     @cocktail = Cocktail.new
@@ -30,16 +28,16 @@ class CocktailsController < ApplicationController
   #   redirect_to cocktails_index_path(@cocktail)
   # end
 
-  # def delete
-  #   @cocktail.destroy
-  #   redirect_to cocktails_show_path(@cocktail)
-  # end
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path(@cocktail)
+  end
 
   private
 
-  # def find_cocktail
-  #   @cocktail = Cocktail.find(params[:id])
-  # end
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
