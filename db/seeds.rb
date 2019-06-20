10422ba184ec5fc93889fcee6e59ec2421b729ef
maya aderka <mayaad0@gmail.com>
2019-06-20 11:58:00 +0300

    added database

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ drinks = JSON.parse(serialized_drink)
 drinks['drinks'].each do |drink|
   Ingredient.create!(name: drink["strIngredient1"])
 end
-# puts '3 more'
-# Ingredient.create(name: 'lemon')
-# Ingredient.create(name: 'ice')
-# Ingredient.create(name: 'mint leaves')
+puts '3 more'
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
 puts 'done'
