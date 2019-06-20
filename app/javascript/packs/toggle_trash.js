38f92e3384ea91b8df5fcbc1b524d19ace855575
maya aderka <mayaad0@gmail.com>
2019-06-20 11:39:54 +0300

    push to heroku

--- app/javascript/packs/toggle_trash.js
+++ app/javascript/packs/toggle_trash.js
@@ -2,7 +2,7 @@
 const toggleTrash = () => {
   const button = document.querySelector(".toggle-trash");
   const cans = document.querySelectorAll(".throw");
-  console.log(cans)
+
   button.addEventListener ('click', () => {
     cans.forEach = ((can) => {
       console.log(can)
